3cfbfa941213c1c8d0e5c1366e9cd63686c36739
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-07-10 10:55:16 +0100

    Navbar done?

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Selectedpage } from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<Selectedpage>(
     Selectedpage.Home
   );
+
+  const [isTopOfpage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(Selectedpage.Home);
+      }
+
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className=" app bg-gray-200 ">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopofPage={isTopOfpage}
+      />
     </div>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -9,16 +9,21 @@ import ActionButton from "@/shared/ActionButton";
 type Props = {
   selectedPage: Selectedpage;
   setSelectedPage: (value: Selectedpage) => void;
+  isTopofPage: boolean;
 };
 
-const index = ({ selectedPage, setSelectedPage }: Props) => {
+const index = ({ selectedPage, setSelectedPage, isTopofPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
+  const navbarBackground = isTopofPage ? "" : "bg-primary-100 drop-shadow";
+
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
@@ -68,7 +73,36 @@ const index = ({ selectedPage, setSelectedPage }: Props) => {
       </div>
 
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl "></div>
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl ">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
       )}
     </nav>
   );
